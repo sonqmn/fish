a2fd47d30a166eac87a40b83e29814c9b820114d
sonqmn <ssong06210914ghd@gmail.com>
2026-08-07 10:18:38 +0900

    Connect Ocean Breathe Firebase authentication

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: 'YOUR_FIREBASE_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.firebasestorage.app',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_FIREBASE_APP_ID'
+  apiKey: 'AIzaSyBVDD4qGrrnptq2yr5AV5jgksnkr5LYmfI',
+  authDomain: 'ocean-breathe-ce596.firebaseapp.com',
+  projectId: 'ocean-breathe-ce596',
+  storageBucket: 'ocean-breathe-ce596.firebasestorage.app',
+  messagingSenderId: '15354827224',
+  appId: '1:15354827224:web:f8f010b504c6e2add122ad'
 };
 
 export const isFirebaseConfigured = !Object.values(firebaseConfig).some(value => value.startsWith('YOUR_'));
